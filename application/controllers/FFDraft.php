8f992b39622531423852ea4c3ecf6652ac39e583
Joel Alexander <alexandj@etl.boi.rd.hpicorp.net>
2017-04-05 16:45:29 -0600

    initial commit

---
+++ application/controllers/FFDraft.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class FFDraft extends CI_Controller {
+    
+    function __construct() {
+        parent::__construct();
+        $this->load->helper("url");
+        self::refresh_ffCalcXML();
+    }
+    
+    public function index(){
+        $this->load->helper('form');
+        $data['title'] = 'FF Draft';
+        $this->load->view('common/head.php', $data);
+        $this->load->view('football/draft.php');
+        $this->load->view('common/foot.php');
+        $this->load->view('football/draft_config.php');
+    }
+
+    private function refresh_ffCalcXML(){
+        $aContext = array(
+            'http' => array(
+            'proxy' => 'web-proxy.boi.hp.com:8080',
+            'request_fulluri' => true,
+            ),
+        );
+        $cxContext = stream_context_create($aContext);
+        $File = file_get_contents("https://fantasyfootballcalculator.com/adp_xml.php", False, $cxContext);
+        if(!file_exists(getcwd() . "\\application\\resources\\xml\\ff_calc_xml.xml") || (time() - filemtime(getcwd() . "\\application\\resources\\xml\\ff_calc_xml.xml") > 3600)){
+            file_put_contents(getcwd() . "\\application\\resources\\xml\\ff_calc_xml.xml", $File);
+            }
+        }
+    }
---
+++ application/views/common/head.php
@@ -0,0 +1,12 @@
+<HTML>
+<TITLE><?php echo $title;?></TITLE>
+<HEAD>
+<!--
+<?php echo "<script src='" . base_url() . "resources/JavaScript/main.js'></script>"; ?>
+<?php echo "<link rel='stylesheet' type='text/css' href='" . base_url() . "resources/css/main.css'>"; ?>
+-->
+
+<?php echo "<script src='/test/resources/JavaScript/main.js'></script>"; ?>
+<?php echo "<link rel='stylesheet' type='text/css' href='resources/css/main.css'>"; ?>
+</HEAD>
+<BODY>
