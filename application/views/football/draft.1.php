38ebf956664feacf012ea60d57becdeda70459d3
Joel Alexander <alexandj@etl.boi.rd.hpicorp.net>
2017-04-22 08:52:22 -0600

    starting new layout branch

---
+++ application/views/football/draft.1.php
@@ -0,0 +1,29 @@
+<div id='page_container'>
+    <div class='menu_container'>
+        <div id='menu_title'>
+            <span>Mocking Draft: Fantasy Football</span>
+            <br>
+            <a href='' id='link_show_draft_config' class='show_draft_config' onclick='show_draft_config(event);'>Configure Draft</a>
+        </div>
+        <div id='menu_body'>
+            <span>some menuy stuff</span>
+        </div>
+        <div id='menu_auth'>
+            <span>login logout</span>
+        </div>
+    </div>
+    <div class='body_container' onclick='hide_draft_config()'>
+
+    <div id='player_list_container'>
+        <div id='player_list_title'>
+                <div>Postion</div>
+                <div>Player Name</div>
+                <div>ADP</div>
+        </div>
+        <div id='player_list'>
+        </div>
+    </div>
+    <div id='team_boards_cont'></div>
+    <div id='draft_history_list'></div>
+    </div>
+</div>
