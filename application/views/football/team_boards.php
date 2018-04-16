5923e7df37bfce4f0839e95566475439b718cf78
Joel Alexander <jt7960@yahoo.com>
2018-04-15 21:48:47 -0600

    fixed the flex problem

--- application/views/football/team_boards.php
+++ application/views/football/team_boards.php
@@ -6,7 +6,7 @@ for($team = 1; $team <= $config->num_teams; $team++){
     echo "<table class='team_board' id='team".$team."board'><tr class='team_board_title_row'><th class='team_board_name' id='team_".$team."_name' colspan='3'>Team ".$team."</th></tr>";
     foreach($config->positions as $position){
         for($i = 1; $i <= $position->quantity; $i++){
-            echo "<tr class='".$position->name."'>
+            echo "<tr class='".str_replace('/', ' ', $position->name)."'>
             <td class='team_board_position'>".$position->name."</td>
             <td class='team_board_player'></td>
             <td class='team_board_pick'></td>
